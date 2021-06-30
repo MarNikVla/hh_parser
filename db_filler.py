5154043c76c5e4866551570033093cd94e88a9a9
marnikvla <condor52@yandex.ru>
2021-06-30 17:43:48 +0300

    minor changes

--- db_filler.py
+++ db_filler.py
@@ -4,31 +4,31 @@ from db_settings import Vacancy, Base
 from parse_single_vacation import parse_single_vacation
 
 def fill_db(vacation_data, vacancy = None):
+    if vacation_data:
 
-    engine = create_engine(f'sqlite:///vacancy-{vacancy}.db')
-    Base.metadata.create_all(engine)
+        engine = create_engine(f'sqlite:///vacancy-{vacancy}.db')
+        Base.metadata.create_all(engine)
 
-    DBSession = sessionmaker(bind=engine)
+        DBSession = sessionmaker(bind=engine)
 
-    session = DBSession()
+        session = DBSession()
 
-    url = 'https://hh.ru/vacancy/45571638'
-
-    res = parse_single_vacation(url)
-    # print(res)
-    # vacancyOne = Vacancy(**res)
-    # print(vacancyOne)
-    session.add(vacation_data)
-    session.commit()
+        session.add(vacation_data)
+        session.commit()
 
 def make_vacation_data(url):
-    res = parse_single_vacation(url)
-    vacancyOne = Vacancy(**res)
+    try:
+        vacancyOne = Vacancy(**parse_single_vacation(url))
+    except AttributeError as e:
+        print(e)
+        return []
+
+    # vacancyOne = Vacancy(**res)
 
     return vacancyOne
 
 if __name__ == '__main__':
     # url = 'https://hh.ru/vacancy/45571638'
-    url = 'https://hh.ru/vacancy/45571638'
+    url = 'https://hh.ru/vacancy/45571111'
     data = make_vacation_data(url)
     fill_db(data, 'программист')
